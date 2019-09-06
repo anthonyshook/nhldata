dea818b8901230fc02353a5b9524387aff816bfd
Anthony Shook <anthonyshook@gmail.com>
2019-09-06 00:42:33 -0500

    Added update script

---
+++ inst/update_database_by_day.R
@@ -0,0 +1,20 @@
+# Database Update Kickoff Script
+
+# Set up a connection
+connection_type <- 'postgres'
+
+cn <- connect_to_db(type = connection_type,
+                    dbname = 'hockey',
+                    schema = 'nhl',
+                    host = 'localhost',
+                    port = 5432,
+                    user = 'postgres',
+                    password = getPass::getPass()
+)
+
+# Set the search path
+DBI::dbSendQuery(cn, statement = 'SET search_path = nhl, public;')
+
+update_nhl_database(look_back_days = 5, conn = cn)
+
+DBI::dbDisconnect(cn)
