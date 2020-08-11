87cdd1410c65b83a91fea6fb28f0641f56b1e793
Anthony Shook <anthonyshook@gmail.com>
2020-08-11 12:49:36 -0500

    description updates

--- inst/update_database_by_day.R
+++ inst/update_database_by_day.R
@@ -3,18 +3,18 @@
 # Set up a connection
 connection_type <- 'postgres'
 
-cn <- connect_to_db(type = connection_type,
-                    dbname = 'hockey',
-                    schema = 'nhl',
-                    host = 'localhost',
-                    port = 5432,
-                    user = 'postgres',
-                    password = Sys.getenv("NHL_PASS") # getPass::getPass()
+cn <- nhldata::connect_to_db(type = connection_type,
+                             dbname = 'hockey',
+                             schema = 'nhl',
+                             host = 'localhost',
+                             port = 5432,
+                             user = 'postgres',
+                             password = Sys.getenv("NHL_PASS") # getPass::getPass()
 )
 
 # Set the search path
 DBI::dbSendQuery(cn, statement = 'SET search_path = nhl, public;')
 
-update_nhl_database(look_back_days = 5, conn = cn)
+nhldata::update_nhl_database(look_back_days = 15, conn = cn)
 
 DBI::dbDisconnect(cn)
