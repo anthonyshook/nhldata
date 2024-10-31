dd93676618462f16967674322479d381903c0825
Anthony Shook <anthonyshook@gmail.com>
2024-10-31 17:19:44 -0500

    Updated to new api endpoints

--- R/api-endpoints.R
+++ R/api-endpoints.R
@@ -2,19 +2,24 @@
 #'  API endpoints
 
 # Game API takes a GAME ID and returns data
-game_api <-  "https://api-web.nhle.com/v1/gamecenter/{{GAME_ID}}/boxscore"
-pbp_api  <-  "https://api-web.nhle.com/v1/gamecenter/{{GAME_ID}}/play-by-play"
+game_api <-  "https://api-web.nhle.com/v1/gamecenter/{GAME_ID}/boxscore"
+pbp_api  <-  "https://api-web.nhle.com/v1/gamecenter/{GAME_ID}/play-by-play"
+game_story_api <- "https://api-web.nhle.com/v1/wsc/game-story/{GAME_ID}"
+landing_api    <- "https://api-web.nhle.com/v1/gamecenter/{GAME_ID}/landing"
 
 # Schedule API.  Endpoints are "NOW" or a specific DATE in YYYY-MM-DD format
 # e.g., https://api-web.nhle.com/v1/schedule/2024-01-23
-schedule_api <- 'https://api-web.nhle.com/v1/schedule/{{DATE}}'
+all_game_ids      <- 'https://api.nhle.com/stats/rest/en/game?cayenneExp=season={SEASON}%20and%20gameType={GAMETYPE}'
+date_schedule_api <- 'https://api-web.nhle.com/v1/schedule/{{DATE}}'
+club_schedule_api <- 'https://api-web.nhle.com/v1/club-schedule-season/{{TEAM}}/{{SEASON}}'
 
-# Teams (list of all available teams)
-teams_api <- "https://api.nhle.com/stats/rest/en/team?"
+# Teams (list of all available teams) -- Can take some report framework stuff
+teams_api <- "https://records.nhl.com/site/api/team"
+franchise_api <-"https://records.nhl.com/site/api/franchise"
 
 # Expanded Rosters, YR to YR
 ## Note -- includes everybody who was on the team, even if not for a full year. (see Nick Bonino)
-roster_api <- 'https://api-web.nhle.com/v1/roster/{{TEAMABBR}}/{{SEASON}}' # https://api-web.nhle.com/v1/roster/PIT/20222023
+roster_api <- 'https://api-web.nhle.com/v1/roster/{TEAM_ABBR}/{SEASON}' # https://api-web.nhle.com/v1/roster/PIT/20222023
 
 # General Player Data, this is pretty much everybody ever.
 all_players_api <- 'https://search.d3.nhle.com/api/v1/search/player?culture=en-us&q=*&limit=100000'
@@ -28,16 +33,16 @@ single_player_api <- 'https://api-web.nhle.com/v1/player/{{PLAYER_ID}}/landing'
 skater_stat_api <- 'https://api.nhle.com/stats/rest/en/skater'
 goalie_stat_api <- "https://api.nhle.com/stats/rest/en/goalie"
 
-# This is a player level Game Log API.
-# Better method might be _boxscore_ data, because that's slightly more reasonable to gather,
-# and you can recreate gamelog from there.
-game_log_api <-
-
 ## Shifts API
-shifts_api <- 'https://api.nhle.com/stats/rest/en/shiftcharts?cayenneExp=gameId='
+shifts_api <- "https://api.nhle.com/stats/rest/en/shiftcharts?cayenneExp=gameId={GAME_ID}"
 
 ## Draft Data
 # You can add YEAR with cayenneExp=draftYear=2023 like this:
 ### https://records.nhl.com/site/api/draft?cayenneExp=draftYear=2023
 ### This goes all the way back to 1963!!
-draft_api = 'https://records.nhl.com/site/api/draft?'
+draft_api <- "https://records.nhl.com/site/api/draft"
+
+## Season / Standings API
+# Gives season start/end dates
+season_api <- "https://api.nhle.com/stats/rest/en/season?cayenneExp=id={SEASON}"
+standings_api <- "https://api-web.nhle.com/v1/standings/{DATE}"
